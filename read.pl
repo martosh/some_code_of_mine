bc9482738069161fa78fa283b60bb0fff0ad1609
martosh <martoshg@gmail.com>
2017-08-11 15:47:27 -0400

    Doc for read.pl

--- read.pl
+++ read.pl
@@ -7,19 +7,13 @@ use Carp;
 use Cwd;
 use CTAH::CSVtok qw(:compat);
 
+
 #########################################################
 # TODO:
 #########################################################
 # General Testing
 # speed testing
-# fix help 
 # See close file handler
-
-#########################################################
-# Developer notes and ideas:
-#########################################################
-# Independent Greps currently the second grep as addition of the others , (greps over the result after the first qrep) 
-#########################################################
 
 # Using the module with @ARGV 
 my $Finder = SeekAndDestroy->new( \@ARGV );
@@ -47,8 +41,7 @@ use Data::Dumper;
 use 5.10.0;
 use Carp;
 use Cwd;
-use lib qw(/historical/testexports/mgrigorov/scripts );
-use mtools qw(d);
+#use lib qw(/historical/testexports/mgrigorov/scripts );
 use File::Find;
 no warnings 'File::Find';
 use File::Path qw(make_path );
@@ -495,6 +488,7 @@ sub read {
 
     my $Options = $self->Options();
     my $fh;
+
     if ($file =~ /.gz$/) {
         open( $fh, "gunzip -c $file |") or confess "Error open pipe to file[$file]";
     } else {
@@ -517,7 +511,7 @@ sub read {
     }
 
 #    close $fh or confess "Error: Cannot close [$file]: [$!]";
-#
+
     return @data;
 }
 
@@ -541,36 +535,183 @@ MGrigorov
 
 MGrigorov 2017-07-25
 
-=head1 Description:
+MGrigorov 2017-08-11
 
-This module is designed wrap Find and QA the historicals report files
+=head1 General Description:
 
-    my $Finder = SeekAndDestroy->new($Options); #see options bellow 
-    my @files = $Finder->get_result();
-    say Dumper \@files; # this will take result 
+This module is designed to wrap File::Find and to do a QA over Fusion historicals report files
+
+The module is separated on two logical parts: finding files and read them for QA report
+	
+    ##### Find files ####
+
+    my $Finder = SeekAndDestroy->new($Options); # Please see options bellow in documentaton Note: Internal Usage for using options in perl
+
+    my @files = $Finder->get_result(); # it will fill @files with founded files for your $Options
+
+    say Dumper \@files; 
+
+    #### QA and report ####
+
+    $Finder->qa(); # This method will perform actual QA and report for founded files 
 
 =head1 Options:
 
-        "dir|d=s@"     => \$Options->{ Dir },          ### Set dir for search files there, it rearches recursively 
-        "o=s"          => \$Options->{ Output },       ### Output file, default is 'qa_result_$time_stamp.txt, if file exist are not replaced but appended
-        "grep|g=s@"    => \$Options->{ Grep },         ### Grep for some files regex
-        "rgrep|gv=s@" => \$Options->{ RGrep },        ### reverse grep for files
-        "help|h"      => \$Options->{ Help },         ### Print Help
-        "grep_in_path"=> \$Options->{ GrepPath },     ### Grep options will use whole path
-        "separator|sep=s"=> \$Options->{ Separator }, ### Define separator for files
-        "do_not_die" => \$Options->{ DoNotDie }, ### Define separator for files
-        "quote=s"=> \$Options->{ Quote },           ### Define Quoting of the files you will check 
-        "head=i"=> \$Options->{ Head },           ### Define how many lines to take from files for qa
-        "line_num=i"=> \$Options->{ OutputLinesForType },           ### Define how many lines to take from files for qa
-=over
+=over 
 
-=head1 Examples:
+=item dir|d 
+	
 
-    perl read.pl -include_dirs -g=script -g=cellid -grep_in_path  # this will find files that are in current dir because there is no -dir option, and will search for script and cellid keyword in files , -grep_in_path gives you a method where the greps are working on the fillpaths otherwise greps are applied only for the basename of the files
+	dir=/var/log d=/etc/ - In witch directory to search recursively. 
+
+	Note:You can give multiple dir options
+
+	Default: if no dir option it will take cwd and it will search recursivly there 
+
+        Internal usage:  $Options->{Dir} = [ /var/log, /home/test/ ];
+
+        Idea: Maybe it will be good idea to be available option with a level how deep to search for it will be quite easy
+
+
+           
+=item o
+
+	o=/home/my_sky_report.txt - Where to put the final report text file
+
+	Default: it will put the file in cwd named as "qa_result_$date_$time.txt";
+
+        Internal usage: $Options->{Output} 
+
+	Note: you can change default name if you don't like it - in constructor $self->{ DefaultOutputFile } = "qa_result_$date_time.txt" 
+
+	Note: if file exists already second start in same time-stamp (same minute) will append the files not replace it
+
+
+
+=item grep|g 
+
+	g=cellid g=test grep=something - Grep allows you to filter finded files with -dir|d option 
+
+        Internal usage: my $Options->{Grep} = [ 'rest', 'test' ];
+
+	Note: every grep|g options is actually used as regular expression so g='test(:?ing)' reg should work 
+	
+	Note: you can give multiple grep arguments and  multiple greps will include files independantly 
+              For example you if you give g=Lepa g=Zvonko it will find both independetly files with Lepa and Zvonko not file that have both patterns
+
+	Note: if you want your greps to be applied in the file paths as well, you will need -grep_in_path option
+	
+	Idea: maybe dependend grep will be good idea for option
+
+
+
+=item rgrep|gv
+
+	rgrep=dont_include_this gv=and_this - this works like reverse grep so omits regex pattern from your found files
+
+        Internal usage: my $Options->{RGrep} = [ 'rest', 'test' ];
+
+	Note: You can give multiple rgrep|gv options.
+
+	Note: Because works as regex if you give rgrep=. this match to everything so it will omit all found files in the given dirs
+
+	Note: if you want your reverse greps to be applied in the file paths as well, you will need -grep_in_path option
+
+
+
+
+=item grep_in_path 
+
+	-grep_in_path - works like flag, when it's turned on allows options grep|g and rgrep|gv to applied regex to the whole path
+
+	Internal usage: my $Options->{GrepPath} = 1;
+
+	Note: Please, take a look at grep|g and rgrep|gv options
+
+
+
+=item do_not_die 
+	
+	-do_not_die - works as flag, and when it's turned on replaces confess function with say usefull to finish your report regarding the problems
+
+
+
+=item separator|sep
+
+	-separator|sep=',' - allow you to give separator for your something separeted files, (format mostly used for the Fusion hitorical reports) 
+
+	Default: If NO separator option given the module will try to guess the separator ( see head option, it's related )
+
+	Internal usage: my $Options->{ Separator } = ',';
+
+	Note: The module will search only in default list with separators $self->{ SeparatorList } = [ '~', ',', '\t' ] you can change it if you want
+
+	Note; The module will work faster if you give that option, not to guess it
+
+
+ 
+=item quote
+
+	-quote='"' - allows you to give the quoting for your readed historical reaports files 
+
+	Default: If NO quote options given the module will try to guess it (see head option, it's related )
+
+	Internal usage: my $Options->{ Quote } = '\"';
+
+	Note: The module will search only in default list with quotes $self->{ QuotingList } = [ "'", "\"" ];
+
+	Note; The module will work faster if you give that option, not to guess it
+
+
+=item head 
+	
+	head=10 - this option wont be useful probably, this is actually related with guessing functionallity.It will take 10 lines for quessing separator or quote
+	
+	Default: for guessing takes first 20 lines foreach file
+
+	Internal usage: my $Options->{Head} = 20;
+
+	Idea: Maybe it will be good to change the default to 10 instead 20
+
+		
+=item line_num
+
+	line_num=5 - the final QA report will print 5 lines for each data type
+
+	Default: Two lines for each data type
+	
+        Internal usage:  my $Options->{ OutputLinesForType } = 5
+
+
+=back
+
+=head1 Methods:
+
+=over 
 
 =item new
 
+	#The constructor recieve custom options and executes the actual File::Find part	
+
+	my $Options = {
+	   Dir => ['/etc', '/home' ],
+	   DoNotDie => 1, 
+	   Grep => [ 'test' ],
+	};
+
+	my $FindAndQA = SeekAndDestroy->new( $Options  );
+
 =back
+
+=head1 Examples:
+
+	
+    perl read.pl -include_dirs -g=script -g=cellid -grep_in_path  
+
+	# this will find files that are in current dir because there is no -dir option, i
+        # It will search for script and cellid keyword in files , 
+        # -grep_in_path with this greps are working on the fillpaths otherwise greps are applied only for the basename of the files
+
 
 =cut 
 
